08c956d0b37127205a2ff65d6e32d7f6b24231e0
Ricci <riccidandrada@gmail.com>
2022-02-03 19:46:48 -0500

    d

--- login.php
+++ login.php
@@ -1,9 +1,29 @@
 
 <?php
 
+function to_middle($data){
+    $middle_curl = "";
+    $username= $_POST['username'];
+    $password= $_POST['password'];
+    $x  = curl_init();
+   curl_setopt($x,CURLOPT_URL,$middle_curl);
+   curl_setopt($x,CURL_POST,strlen($data));
+   curl_setopt($x,CURL_POSTFIELDS,$data);
+   curl_setopt($x,CURLOPT_RETURNTRANSFER,true);
 
-    var_dump($_POST);
+   $responseData = curl_exec($x);
+   curl_close($x);
 
+
+   return $responseData
+
+
+
+}
+$middle_response = to_middle(formData);
+
+
+echo $middle_response;
 
 
 ?>
